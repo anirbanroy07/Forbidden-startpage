f5f27c0b0e20fa14ef63b64f8db4a522d98d44a6
anirbanroy07 <imanirbanroy88@gmail.com>
2025-02-22 19:45:29 +0530

    Changed The Css And JS code

--- script/themes.js
+++ script/themes.js
@@ -1,28 +1,3 @@
-<<<<<<< HEADgit 
-themeRandomizer = () => {
-const theme = ["gold", "pastel", "alcest", "griffith", "guts"]
-const random = theme[Math.floor(Math.random() * theme.length)]
-return random
-}
-
-setTheme = () => {
-    const selector = document.getElementsByClassName("image")[0]
-    savedTheme = localStorage.getItem('theme')
-    if(savedTheme) {
-        document.documentElement.className = savedTheme
-    }
-
-
-
-    selector.addEventListener("click", () => {
-        theme = themeRandomizer()
-        document.documentElement.className = theme
-        localStorage.setItem('theme', theme)
-    })
-}
-
-setTheme()
-=======
 // List of themes in order
 const themes = ["gold", "pastel", "alcest", "griffith", "guts"];
 
@@ -52,4 +27,3 @@ const setTheme = () => {
 
 // Ensu runs after DOM is fully loaded
 document.addEventListener("DOMContentLoaded", setTheme);
->>>>>>> 666134c (Changed The Css And JS code)
